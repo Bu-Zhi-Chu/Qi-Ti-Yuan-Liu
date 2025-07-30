e292d34a8a8fbc0f99e84dd093518985804b86d5
步知处 <2636295313@qq.com>
2025-07-30 17:13:17 +0800

    chore: 在.gitignore中添加dev-dist目录
    防止开发构建输出目录被提交到版本控制

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qq5f81dleno"
+    "revision": "0.p52ig65inrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
