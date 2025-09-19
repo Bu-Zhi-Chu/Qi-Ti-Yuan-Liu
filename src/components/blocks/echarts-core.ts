19e31ac8c6820804ad5653cb243b13630d8f940d
步知处 <2636295313@qq.com>
2025-09-19 17:08:18 +0800

    feat(echarts-core): 添加更多 ECharts 组件以增强图表功能
    添加 MarkPointComponent、MarkLineComponent、ToolboxComponent 和 DataZoomComponent 组件，支持图表标记点、标记线、工具箱和数据缩放功能

--- src/components/blocks/echarts-core.ts
+++ src/components/blocks/echarts-core.ts
@@ -6,7 +6,7 @@ import * as echarts from 'echarts/core'
 // 图表类型
 import { BarChart, LineChart, PieChart } from 'echarts/charts'
 // 组件
-import { GridComponent, TooltipComponent, LegendComponent, TitleComponent } from 'echarts/components'
+import { GridComponent, TooltipComponent, LegendComponent, TitleComponent, MarkPointComponent, MarkLineComponent, ToolboxComponent, DataZoomComponent } from 'echarts/components'
 // 渲染器
 import { CanvasRenderer } from 'echarts/renderers'
 
@@ -19,6 +19,10 @@ echarts.use([
   TooltipComponent,
   LegendComponent,
   TitleComponent,
+  MarkPointComponent,
+  MarkLineComponent,
+  ToolboxComponent,
+  DataZoomComponent,
   CanvasRenderer
 ])
 
